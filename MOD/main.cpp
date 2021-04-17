09aec88e1fd761a81009dec34a9c2f76d92748a7
dsaf2007 <dsaf2007@naver.com>
2021-04-18 01:00:37 +0900

    Update main.cpp
    solve power problem

--- MOD/main.cpp
+++ MOD/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
 #include <fstream>
 #include <time.h>
 #include <chrono>
+#include <random>
 int gdb(int a, int b)
 {
 	int temp, x = a, y = b;
@@ -18,12 +19,19 @@ int gdb(int a, int b)
 int randPrime(int n)
 {
 
-	int a = rand();
+	int a =  rand();
 	while (gdb(a, n) != 1)
 	{
 		a = rand();
 	}
 	return a;
+}
+
+long long pow(long long k, int x, int N) {
+	if (x == 0) return 1;
+	if (x == 1) return k;
+	if (x % 2 == 1) return (k * pow(k * k % N, x / 2, N)) % N;
+	if (x % 2 == 0) return pow(k * k % N, x / 2, N) % N;
 }
 int main(void)
 {
@@ -32,26 +40,36 @@ int main(void)
 	std::cout.tie(NULL);
 	srand(time(NULL));
 	int N;
-	int rangeMax = 99999;
-	int rangeMin = 10000;
-	std::ofstream output("./test_6.txt");
+	int rangeMax = 9999;
+	int rangeMin = 1000;
+	std::ofstream output("./test_4.txt");
 	clock_t start, end;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dis(rangeMin, rangeMax);
+	int R[100]; 
+	for (int i = 0; i < 100; i++)
+	{
+		R[i] = dis(gen);
+		std::cout << i<<" "<<R[i] << std::endl;
+	}
 
 	for (int i = 0; i < 100; i++)
 	{
-		N = (int)(((double)((rand() << 15) | rand())) / (((RAND_MAX << 15) | RAND_MAX) + 1) * (rangeMax - rangeMin) + rangeMin);
 
-		int k = randPrime(N);
-
+		std::chrono::system_clock::time_point StartTime = std::chrono::system_clock::now();
+		int k = randPrime(R[i]);
 		int x = 1;
-		std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-		for (int result = k % N; result != 1; ++x)
+		for (int result = k % R[i]; result != 1; ++x)
 		{
-			result = (result * k) % N;
+			result = pow(result, x, R[i]);
+
 		}
-		std::chrono::duration<double> sec = std::chrono::system_clock::now() - start;
-		std::cout << "<" << i << ">" << k << "^" << x << " mod " << N << " = 1 | time : " << sec.count() << "ÃÊ\n";
-		output << "<" << i << ">" << k << "^" << x << " mod " << N << " = 1 | time : " << sec.count() << "ÃÊ\n";
+		
+		std::chrono::system_clock::time_point EndTime = std::chrono::system_clock::now();
+		std::chrono::nanoseconds mill = std::chrono::duration_cast<std::chrono::nanoseconds>(EndTime - StartTime);
+		std::cout << "<" << i << ">" << k << "^" << x << " mod " << R[i] << " = 1 | time : " << mill.count() << "nanosec\n";
+		output << "<" << i << ">" << k << "^" << x << " mod " << R[i] << " = 1 | time : " << mill.count() << "nanosec\n";
 	}
 
 	return 0;
